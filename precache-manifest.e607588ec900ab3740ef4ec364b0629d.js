55a9dd4dc7430d8a1c16b2f87f040ab663c8873b
Dan Fitz <dan@danfitz.com>
2020-02-05 11:40:15 -0500

    Updates

---
+++ precache-manifest.e607588ec900ab3740ef4ec364b0629d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f036eb4f648c17f25e79b3b5f5e2d359",
+    "url": "/CityBikes/index.html"
+  },
+  {
+    "revision": "27f0017caf132e2e5541",
+    "url": "/CityBikes/static/css/main.0a7d0319.chunk.css"
+  },
+  {
+    "revision": "6443b48040c118579b7f",
+    "url": "/CityBikes/static/js/2.5bb2e150.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CityBikes/static/js/2.5bb2e150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f0017caf132e2e5541",
+    "url": "/CityBikes/static/js/main.cbb5a47c.chunk.js"
+  },
+  {
+    "revision": "2a0012d4956afce3a2da",
+    "url": "/CityBikes/static/js/runtime-main.9665e1d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kanusobti/CityBikes/precache-manifest.a18318f2035366ad8e25376ea04444f5.js"
+  "/CityBikes/precache-manifest.e607588ec900ab3740ef4ec364b0629d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kanusobti/CityBikes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CityBikes/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
